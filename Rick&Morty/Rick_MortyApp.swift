45f454e378b9f34b5864ad5857150262488b5dc1
Marc Hidalgo <marc@theleftbit.com>
2023-11-02 16:24:33 +0100

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Marc Hidalgo on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
